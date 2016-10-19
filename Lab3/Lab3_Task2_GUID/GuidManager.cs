990f24bb897ac51cdc03cf6b528aaf94f83484d0
Michael <master8.michael@yahoo.com>
2016-10-19 23:12:23 +0500

    Lab3 Task2 Guid

---
+++ Lab3/Lab3_Task2_GUID/GuidManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Task2_GUID
+{
+    class GuidManager
+    {
+        Dictionary<Type, Dictionary<Guid, object>> dictionary = new Dictionary<Type, Dictionary<Guid, object>>();
+
+        public T Create<T>() where T : new()
+        {
+            T obj = new T();
+
+            if (dictionary.ContainsKey(typeof(T)))
+            {
+                dictionary[typeof(T)].Add(Guid.NewGuid(), obj);
+            }
+            else
+            {
+                dictionary.Add(typeof(T), new Dictionary<Guid, object>() { { Guid.NewGuid(), obj } });
+            }
+
+            return obj;
+        }
+
+        public Dictionary<Guid, object> GetObjectsForType<T>()
+        {
+            if (dictionary.ContainsKey(typeof(T)))
+                return dictionary[typeof(T)];
+            else
+                return null;
+        }
+
+        public T GetObject<T>(Guid guid)
+        {
+            if (dictionary.ContainsKey(typeof(T)))
+            {
+                var objects = dictionary[typeof(T)];
+
+                if (objects.ContainsKey(guid))
+                {
+                    return (T)objects[guid];
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+    }
+}
---
+++ Lab3/Lab3_Task2_GUID/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Lab3_Task2_GUID")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab3_Task2_GUID")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1046300c-059f-4bbe-8f2d-d8049a1b6277")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
