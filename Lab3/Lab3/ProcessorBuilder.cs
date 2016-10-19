cfc7dce1dea45b0c457fe19e64bdbce7bbe900fa
Michael <master8.michael@yahoo.com>
2016-10-19 21:41:06 +0500

    Lab3 Task1

---
+++ Lab3/Lab3/ProcessorBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class ProcessorBuilder
+    {
+        public static ProcessorEngine<TEngine> CreateEngine<TEngine>()
+        {
+            return new ProcessorEngine<TEngine>();
+        }
+    }
+
+    class ProcessorEngine<TEngine>
+    {
+        public ProcessorEntity<TEngine, TEntity> For<TEntity>()
+        {
+            return new ProcessorEntity<TEngine, TEntity>();
+        }
+    }
+
+    class ProcessorEntity<TEngine, TEntity>
+    {
+        public Processor<TEngine, TEntity, TLogger> With<TLogger>()
+        {
+            return new Processor<TEngine, TEntity, TLogger>();
+        }
+    }
+}
