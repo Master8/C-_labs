882a1d182ef1631e78d67342329f0976be5efa15
Michael <master8.michael@yahoo.com>
2016-10-28 13:41:02 +0500

    Lab4 Task2

---
+++ Lab4/Lab4/Processor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Processor<TRequest, TResult>
+    {
+        private Func<TRequest, bool> check;
+        private Func<TRequest, TResult> register;
+        private Action<TResult> save;
+
+        public Processor(Func<TRequest, bool> check, Func<TRequest, TResult> register, Action<TResult> save)
+        {
+            this.check = check;
+            this.register = register;
+            this.save = save;
+        }
+
+        public TResult Process(TRequest request)
+        {
+            if (!check(request))
+                throw new ArgumentException();
+            var result = register(request);
+            save(result);
+            return result;
+        }
+    }
+}
