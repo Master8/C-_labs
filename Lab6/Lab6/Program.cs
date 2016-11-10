9b13f33cd6f7043c8f52a75f7cb7dba6df604cd2
Michael <master8.michael@yahoo.com>
2016-11-10 14:15:32 +0500

    Lab6 Task1

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer();
+            using (timer.Start())
+            {
+                // do things
+                Thread.Sleep(100);
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            using (timer.Continue())
+            {
+                // do things
+                Thread.Sleep(100);
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Lab6/Lab6/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace Lab6
+{
+    class Timer
+    {
+        private Stopwatch stopWatch = new Stopwatch();
+
+        public long ElapsedMilliseconds
+        {
+            get
+            {
+                return stopWatch.ElapsedMilliseconds;
+            }
+        }
+
+        public TimerToken Start()
+        {
+            stopWatch.Start();
+            return new TimerToken(stopWatch);
+        }
+
+        public TimerToken Continue()
+        {
+            stopWatch.Start();
+            return new TimerToken(stopWatch);
+        }
+
+        public class TimerToken : IDisposable
+        {
+            private Stopwatch stopWatch;
+
+            public TimerToken(Stopwatch stopWatch)
+            {
+                this.stopWatch = stopWatch;
+            }
+
+            public void Dispose()
+            {
+                stopWatch.Stop();
+            }
+        }
+    }
+}
