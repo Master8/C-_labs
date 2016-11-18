47ee6a933e09515662caaa48baeefd7c0d5489f7
Michael <master8.michael@yahoo.com>
2016-11-18 20:20:26 +0500

    Lab7 Task3

--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -112,7 +112,71 @@ namespace Lab7
                 }
             }
         }
-        
+
+        //Task 3
+        static IEnumerable<Dictionary<string, object>> ReadCsv3(string fileName)
+        {
+            using (var stream = new StreamReader(fileName))
+            {
+                var line = stream.ReadLine();
+
+                if (line == null)
+                {
+                    stream.Close();
+                    yield break;
+                }
+
+                string[] colums = line.Split(',');
+
+                for (int i = 0; i < colums.Length; i++)
+                {
+                    string value = colums[i];
+
+                    if (value.Contains("."))
+                        colums[i] = value.Replace(".", "").Trim('\"');
+                    else
+                        colums[i] = value.Trim('\"');
+                }
+
+                while (true)
+                {
+                    line = stream.ReadLine();
+
+                    if (line == null)
+                    {
+                        stream.Close();
+                        yield break;
+                    }
+
+                    Dictionary<string, object> result = new Dictionary<string, object>();
+                    var values = line.Split(',');
+
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        string value = values[i].Trim('\"');
+
+                        if (value == "NA")
+                        {
+                            result.Add(colums[i], null);
+                        }
+                        else
+                        {
+                            double dValue;
+                            int iValue;
+
+                            if (double.TryParse(value, out dValue))
+                                result.Add(colums[i], dValue);
+                            else if (int.TryParse(value, out iValue))
+                                result.Add(colums[i], iValue);
+                            else
+                                result.Add(colums[i], value);
+                        }
+                    }
+
+                    yield return result;
+                }
+            }
+        }
         static void Main(string[] args)
         {
             //Task 1
@@ -126,6 +190,12 @@ namespace Lab7
             foreach (Line line in ReadCSV<Line>(@"C:\Users\maste\Downloads\\airquality.csv"))
             {
                 Console.WriteLine(line.Name + "\t" + line.SolarR + "\t" + line.Ozone);
+            }
+
+            //Task 3
+            foreach (var line in ReadCsv3(@"C:\Users\maste\Downloads\\airquality.csv"))
+            {
+                Console.WriteLine(line["Name"] + "\t" + line["Ozone"] + "\t" + line["Wind"]);
             }
         }
     }
